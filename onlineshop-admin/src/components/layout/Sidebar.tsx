61c0008f0dc8fad1f5f54e39828a158ceaa96c9b
Night <dranhata@gmail.com>
2024-09-07 10:50:35 +0700

    update sidebar for products

--- onlineshop-admin/src/components/layout/Sidebar.tsx
+++ onlineshop-admin/src/components/layout/Sidebar.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 
-import { FaArrowLeft, FaListUl, FaHandHolding } from 'react-icons/fa'
+import { FaArrowLeft, FaListUl, FaHandHolding, FaProductHunt, FaAngleUp, FaAngleDown } from 'react-icons/fa'
 import Logo from '~/assets/logo.png'
+import SidebarLinkGroup from './SidebarLinkGroup'
 
 interface SidebarProps {
   sidebarOpen: boolean
@@ -17,7 +18,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   const trigger = useRef<HTMLButtonElement>(null)
 
   const storedSidebarExpanded = localStorage.getItem('sidebar-expanded')
-  const [sidebarExpanded] = useState(storedSidebarExpanded === null ? false : storedSidebarExpanded === 'true')
+  const [sidebarExpanded, setSidebarExpanded] = useState(
+    storedSidebarExpanded === null ? false : storedSidebarExpanded === 'true'
+  )
 
   // close on click outside
   useEffect(() => {
@@ -59,7 +62,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
-      <div className='flex items-center justify-between gap-2 px-6 py-5 lg:py-6'>
+      <div className='flex items-center justify-between gap-3 px-6 py-5 lg:py-6'>
         <NavLink to='/' className='flex items-center justify-between gap-2'>
           <img src={Logo} alt='Logo' />
           <span className='text-4xl font-semibold'>Shop</span>
@@ -80,7 +83,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       <div className='scrollbar flex flex-col overflow-y-auto duration-300 ease-linear'>
         {/* <!-- Sidebar Menu --> */}
         <nav className='mt-5 py-4 px-4 lg:mt-9 lg:px-6 text-xl'>
-          <ul className='mb-6 flex flex-col gap-1'>
+          <ul className='mb-6 flex flex-col gap-2.5'>
             {/* <!-- Menu Item Category --> */}
             <li>
               <NavLink
@@ -107,6 +110,58 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               </NavLink>
             </li>
             {/* <!-- Menu Item Brand --> */}
+            {/* <!-- Menu Item Forms --> */}
+            <SidebarLinkGroup activeCondition={pathname === '/products' || pathname.includes('products')}>
+              {(handleClick, open) => {
+                return (
+                  <>
+                    <NavLink
+                      to='#'
+                      className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium duration-300 ease-in-out hover:bg-yellow-500 ${
+                        (pathname === '/products' || pathname.includes('products')) && 'bg-yellow-500'
+                      }`}
+                      onClick={(e) => {
+                        e.preventDefault()
+                        sidebarExpanded ? handleClick() : setSidebarExpanded(true)
+                      }}
+                    >
+                      <FaProductHunt />
+                      Sản phẩm
+                      {open ? <FaAngleUp className='ms-auto' /> : <FaAngleDown className='ms-auto' />}
+                    </NavLink>
+                    {/* <!-- Dropdown Menu Start --> */}
+                    <div className={`translate transform overflow-hidden ${!open && 'hidden'}`}>
+                      <ul className='mt-4 mb-5.5 flex flex-col gap-2.5 pl-6'>
+                        <li>
+                          <NavLink
+                            to='/products/list'
+                            className={({ isActive }) =>
+                              'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-slate-300 duration-300 ease-in-out hover:text-white ' +
+                              (isActive && '!text-yellow-400 cursor-default')
+                            }
+                          >
+                            Danh sách
+                          </NavLink>
+                        </li>
+                        <li>
+                          <NavLink
+                            to='/products/new'
+                            className={({ isActive }) =>
+                              'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-slate-300 duration-300 ease-in-out hover:text-white ' +
+                              (isActive && '!text-yellow-400 cursor-default')
+                            }
+                          >
+                            Tạo Sản Phẩm
+                          </NavLink>
+                        </li>
+                      </ul>
+                    </div>
+                    {/* <!-- Dropdown Menu End --> */}
+                  </>
+                )
+              }}
+            </SidebarLinkGroup>
+            {/* <!-- Menu Item Forms --> */}
           </ul>
         </nav>
       </div>
